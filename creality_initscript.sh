68e06e07aac99128a9807583cb552ba604251d31
Sascha Felbecker <thrinrot+github@gmail.com>
2023-10-09 19:22:25 +0200

    added moonraker.conf

--- creality_initscript.sh
+++ creality_initscript.sh
@@ -4,14 +4,16 @@ echo -e "creality\ncreality" | passwd
 
 cd /usr/data
 wget -q -O mrngx.tar https://github.com/fir3bird/K1/raw/main/moonraker_nginx.tar
-tar -xvf mrngx.tar && rm mrngx.tar
-cp moonraker/S56moonraker_service /etc/init.d/S56moonraker_service
-cp /usr/data/nginx/S50nginx /etc/init.d/S50nginx
+tar -xf mrngx.tar && rm mrngx.tar
+cp -f moonraker/S56moonraker_service /etc/init.d/S56moonraker_service
+cp -f /usr/data/nginx/S50nginx /etc/init.d/S50nginx
 git clone https://github.com/Arksine/moonraker.git moonraker/moonraker
 ./moonraker/moonraker-env/bin/pip install -r ./moonraker/scripts/moonraker-requirements.txt
 
+mkdir mainsail && cd mainsail
 wget -q -O mainsail.zip https://github.com/mainsail-crew/mainsail/releases/latest/download/mainsail.zip && unzip mainsail.zip && rm mainsail.zip
 
+cd /usr/data
 wget -q -O nginx.conf https://github.com/fir3bird/K1/raw/main/nginx.conf && mv nginx/nginx/nginx.conf nginx/nginx/nginx.conf.bak && cp nginx.conf nginx/nginx/nginx.conf
 
 sed -i 's/[ -x "$BIN_PATH\/$WEB_SERVER" ] && $BIN_PATH\/$WEB_SERVER &/#[ -x "$BIN_PATH\/$WEB_SERVER" ] && $BIN_PATH\/$WEB_SERVER &/g' /etc/init.d/S99start_app
